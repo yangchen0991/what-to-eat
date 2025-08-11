bca0895edeaf1a762ad197deb0c828a7a53c65d6
liuziting <liuziting2014@163.com>
2025-08-11 17:19:47 +0800

    优化部署问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import path from 'path'
 
 export default defineConfig({
     plugins: [vue()],
     resolve: {
         alias: {
-            '@': resolve(__dirname, 'src')
+            '@': path.resolve(__dirname, 'src')
         }
+    },
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        sourcemap: false,
+        minify: 'terser'
     }
 })
